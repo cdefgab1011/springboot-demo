f29d89e5247165ca585ccc054294d88f07479a3f
cdefgab1011@gmail.com <cdefgab1011@gmail.com>
2021-10-24 11:54:41 +0800

    优化swagger文档

--- src/main/java/com/example/demo/controller/WeatherController.java
+++ src/main/java/com/example/demo/controller/WeatherController.java
@@ -2,8 +2,9 @@ package com.example.demo.controller;
 
 import com.example.demo.entity.Weather;
 import com.example.demo.service.WeatherService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,46 +16,25 @@ public class WeatherController {
 
     private final WeatherService weatherService;
 
-    /**
-     * create database and table
-     * @return
-     */
     @GetMapping("/init")
-    public boolean init(){
+    @ApiOperation(value = "初始化数据库和表", notes = "备注")
+    public boolean init() {
         return weatherService.init();
     }
 
-    /**
-     * Pagination Query
-     * @param limit
-     * @param offset
-     * @return
-     */
     @GetMapping("/{limit}/{offset}")
-    public List<Weather> queryWeather(@PathVariable Long limit, @PathVariable Long offset){
+    public List<Weather> queryWeather(@PathVariable Long limit, @PathVariable Long offset) {
         return weatherService.query(limit, offset);
     }
 
-    /**
-     * upload single weather info
-     * @param temperature
-     * @param humidity
-     * @return
-     */
     @GetMapping("/save/{temperature}/{humidity}")
-    public int saveWeather(@PathVariable int temperature, @PathVariable float humidity){
+    public int saveWeather(@PathVariable int temperature, @PathVariable float humidity) {
 
         return weatherService.save(temperature, humidity);
     }
 
-    /**
-     * upload multi weather info
-     * @param weatherList
-     * @return
-     */
     @PostMapping("/batch")
-    public int batchSaveWeather(@RequestBody List<Weather> weatherList){
-
+    public int batchSaveWeather(@RequestBody List<Weather> weatherList) {
         return weatherService.save(weatherList);
     }
 
--- src/main/java/com/example/demo/entity/Weather.java
+++ src/main/java/com/example/demo/entity/Weather.java
@@ -1,5 +1,7 @@
 package com.example.demo.entity;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,14 +9,18 @@ import lombok.NoArgsConstructor;
 import java.util.Date;
 
 @Data
+@ApiModel
 @AllArgsConstructor
 @NoArgsConstructor
 public class Weather {
 
+    @ApiModelProperty(value = "时间戳", position = 1, required = true)
     private Date ts;
 
+    @ApiModelProperty(value = "温度", position = 2, required = true)
     private int temperature;
 
+    @ApiModelProperty(value = "湿度", position = 3)
     private float humidity;
 
 }
